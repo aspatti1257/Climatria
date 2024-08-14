ca4780ee72b9a24f12f294bd3407d7c842bba858
Austen Sorochak <austensorochak@gmail.com>
2024-08-13 20:06:35 -0700

    update handleSubmit to interact with API endpoint

--- src/climatriaUI/src/pages/Home.jsx
+++ src/climatriaUI/src/pages/Home.jsx
@@ -56,9 +56,24 @@ function Home() {
     }));
   };
 
-  const handleSubmit = () => {
-    // API call can be made here with formData
-    console.log(formData);
+  const handleSubmit = async () => {
+    try {
+      const response = await fetch("http://127.0.0.1:8080/api/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (response.ok) {
+        console.log("User signed up successfully");
+      } else {
+        console.error("Failed to sign up user");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
   };
 
   return (
