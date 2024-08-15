7a6454bcbfc23268fdb900ba9fc130086c5d1980
Austen Sorochak <austensorochak@gmail.com>
2024-08-15 00:11:27 -0700

    Dockerize frontend and API

--- src/climatriaUI/vite.config.js
+++ src/climatriaUI/vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: "0.0.0.0", // Listen on all addresses, necessary for Docker
+    port: 5173, // Make sure this matches the port you expose in Docker
+    strictPort: true, // Fail if the port is already in use
+  },
+});
