cd4c9f09976525be4071abc518544157ddbb9e40
aspatti1257 <aspatti1257@gmail.com>
2024-08-14 20:07:17 -0400

    Hook up creds to hit Mongo directly from hosted app.

--- src/backend/app.py
+++ src/backend/app.py
@@ -16,12 +16,21 @@ app = Flask(__name__)
 # Register the blueprint
 app.register_blueprint(user_blueprint)
 
+
 def setup_flask():
     local_host = "127.0.0.1"
     port = 8080
     log.info("Setting up the Flask application on host: %s and port %s.", local_host, port)
 
     app.run(host=local_host, port=port, debug=True)
+
+
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+    return response
 
 
 @app.route('/', methods=['GET'])
